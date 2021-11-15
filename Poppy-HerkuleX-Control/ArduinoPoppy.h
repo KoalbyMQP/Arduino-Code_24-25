a82d58ad5f1898d7eea7cdfa9062b7e704cf83c2
agalgano08 <anthony.galgano@gmail.com>
2021-11-15 14:49:08 -0500

    Restructure Done

--- Poppy-HerkuleX-Control/ArduinoPoppy.h
+++ Poppy-HerkuleX-Control/ArduinoPoppy.h
@@ -2,11 +2,17 @@
 #define ArduinoPoppy_h
 #include <Herkulex.h>
 
-
 // the #include statment and code go here...
 class ArduinoPoppy {
   public:
     ArduinoPoppy();
+    void Setup();
+    int ReadCommand();
+    void Initialize(int idArr[][4]);
+    void GetPosition();
+    void SetPosition(int idArr[][4]);
+    void SetPositionT(int idArr[][4]);
+    void ArmMirror(int idArr[][4], int mirrorArray[4][2], bool armMirrorModeOn, int lastMirror);
     void PuppetMaster();
   private:
 
