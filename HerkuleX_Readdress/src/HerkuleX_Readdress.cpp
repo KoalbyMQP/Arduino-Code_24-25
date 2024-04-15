513ba464ddbcf09ca3003e9b490ab90be1dcaf76
sfanning5 <stephen.e.fanning@gmail.com>
2024-04-14 20:58:16 -0400

    Prevented future heart attacks

--- HerkuleX_Readdress/src/HerkuleX_Readdress.cpp
+++ HerkuleX_Readdress/src/HerkuleX_Readdress.cpp
@@ -1,8 +1,8 @@
 #include <Herkulex.h>
 
 //NEVER RUN THIS PROGRAM ON THE ACTUAL ROBOT IT WILL RESET EVERYTHING
-int oldID = 100; //set the motor ID - cant be FE
-int newID = 101; //set the motor ID
+int oldID = 253; //set the motor ID - cant be FE
+int newID = 13; //set the motor ID
 bool is0601 = false;
 
 void setup()  
@@ -10,7 +10,7 @@ void setup()
   delay(2000);  //a delay to have time for serial monitor opening
   Serial.begin(115200);    // Open serial communications
   Serial.println("Begin");
-  Herkulex.beginSerial3(115200); //open serial port 2
+  Herkulex.beginSerial2(115200); //open serial port 2
   Herkulex.reboot(0xfe); //reboot 
   delay(500); 
   Herkulex.initialize(); //initialize motors
@@ -27,12 +27,12 @@ void setup()
   delay(500); 
   Herkulex.initialize(); //initialize motors
 
-  Herkulex.setLed(0xfe, LED_RED);
+  Herkulex.setLed(0xfe, LED_BLUE);
   delay(10);
   Herkulex.setLed(newID, LED_GREEN);
   Serial.println("Done changing id");
 }
 
 void loop() {
-   Serial.println(Herkulex.getAngle(newID, is0601));
+   Serial.println(Herkulex.stat(newID));
 }
