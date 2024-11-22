4b7a0da83cd8c9c1b088f1d8b78c085a6258ecb6
Joseph Nixon <nixonjosephb@gmail.com>
2024-11-22 16:34:32 -0500

    code to clear error on one herkulex motor and move angle

---
+++ test/io/outputs/actuators/herkulex/clearError/clearErrorTestCode/clearErrorTestCode.ino
@@ -0,0 +1,37 @@
+#include <Herkulex.h>
+int n=6; //motor ID - verify your ID !!!!
+
+void setup()  
+{
+  delay(2000);  //a delay to have time for serial monitor opening
+  Serial.begin(57600);   // Open serial communications
+  Serial.println("Begin");
+  // Herkulex.beginSerial1(57600); //open serial port 1
+  Herkulex.begin(57600,18,19);
+  Herkulex.reboot(n); //reboot first motor
+  delay(500); 
+  Herkulex.initialize(); //initialize motors
+  delay(200);  
+  Herkulex.clearError(n);
+  Serial.println("Clearing Motor Errors on Reboot...");
+  
+}
+
+void loop(){
+
+  Serial.println("Move Angle: -100 degrees");
+  Herkulex.moveOneAngle(n, -100, 1000, LED_BLUE); //move motor with 300 speed  
+  delay(1200);
+  // Serial.print("Get servo Angle:");
+  // Serial.println(Herkulex.getAngle(n));
+  Serial.println("Move Angle: 100 degrees");
+  Herkulex.moveOneAngle(n, 100, 1000, LED_GREEN); //move motor with 300 speed  
+  delay(1200);
+  //Herkulex.clearError(n);
+  Serial.println(Herkulex.stat(n));
+  // Serial.print("Get servo Angle:");
+  // Serial.println(Herkulex.getAngle(n));
+  
+}
+
+
