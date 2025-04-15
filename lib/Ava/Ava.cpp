d723bf09880f039af5adecd250214d074a60cf21
Lily Jones <127898963+Lily7jones@users.noreply.github.com>
2025-04-15 12:30:08 -0400

    tpose 4/15 and getting rid of motor 17/18

--- lib/Ava/Ava.cpp
+++ lib/Ava/Ava.cpp
@@ -9,7 +9,7 @@ Motor Left_Arm_Abductor = {3, -200, 160, 36, true, "Left_Arm_Abductor"}; // limi
 // Right Arm (Wrist to Shoulder)
 Motor Right_Wrist_Abductor = {25, -160, 160, 65, false, "Right_Wrist_Abductor"};
 Motor Right_Elbow = {11, -160, 160, 150, true, "Right_Elbow"};
-Motor Right_Arm_Rotator = {10, -160, 160, -29, false, "Right_Arm_Rotator"};  // changed 10 to 0 
+Motor Right_Arm_Rotator = {10, -160, 160, -29, false, "Right_Arm_Rotator"};   
 Motor Right_Arm_Abductor = {6, -200, 160, -10, true, "Right_Arm_Abductor"};
 
 // Chest/Neck (Top to Bottom, Left to Right, Front to Back)
@@ -17,15 +17,16 @@ Motor Top_Neck = {28, -160, 160, 48, false, "Top_Neck"}; //not connected
 Motor Bottom_Neck = {27, -160, 160, -96, false, "Bottom_Neck"}; //not connected 
 Motor Left_Shoulder = {7, -160, 160, -64, true, "Left_Shoulder"};
 Motor Right_Shoulder = {15, -160, 160, -51, true, "Right_Shoulder"};
+//motors taken out in MQP 24/25 and no longer used 
 Motor Front_Chest = {18, -160, 160, 0, false, "Front_Chest"}; //taken out
 Motor Back_Chest = {17, -160, 160, -62, false, "Back_Chest"}; //taken out
 
 // Pelvis (Top to Bottom, Left to Right, Front to Back
 Motor Hips_Rotate_Upper_Body = {19, -160, 160, -70, true, "Hips_Rotate_Upper_Body"}; 
 Motor Hips_Lean_Side_To_Side = {21, -160, 160, -31, true, "Hips_Lean_Side_To_Side"};
-Motor Hips_Bend_Over = {22, -38, 50, -38, true, "Hips_Bend_Over"};
+Motor Hips_Bend_Over = {22, -38, 50, -25, true, "Hips_Bend_Over"};
 Motor Left_Leg_Abductor_Side_To_Side = {8, -160, 160, -98, true, "Left_Leg_Abductor_Side_To_Side"}; 
-Motor Right_Leg_Abductor_Side_To_Side = {9, -160, 160, -49, true, "Right_Leg_Abductor_Side_To_Side"};
+Motor Right_Leg_Abductor_Side_To_Side = {9, -160, 160, -55, true, "Right_Leg_Abductor_Side_To_Side"};
 
 // Left Leg (Foot to Hip)
 Motor Left_Leg_Rotator = {14, -160, 160, -18, true, "Left_Leg_Rotator"};
@@ -46,7 +47,7 @@ Motor motors[MOTOR_COUNT] = {
     Left_Elbow, Left_Arm_Rotator, Left_Arm_Abductor, Right_Leg_Rotator, Right_Ankle,                                       // 1-5
     Right_Arm_Abductor, Left_Shoulder, Left_Leg_Abductor_Side_To_Side, Right_Leg_Abductor_Side_To_Side, Right_Arm_Rotator, // 6-10
     Right_Elbow, Left_Knee, Left_Ankle, Left_Leg_Rotator, Right_Shoulder,                                                  // 11-15
-    Back_Chest, Front_Chest, Hips_Rotate_Upper_Body, Right_Knee, Hips_Lean_Side_To_Side,                                   // 17 - 21
+    Hips_Rotate_Upper_Body, Right_Knee, Hips_Lean_Side_To_Side,                                   // 17 - 21
     Hips_Bend_Over, Right_Wrist_Abductor, Left_Wrist_Abductor, Bottom_Neck, Top_Neck,                                      // 22, 25-28
     Left_Leg_Abductor_Front_To_Back, Right_Leg_Abductor_Front_To_Back                                                      // 30-31
 };
--- lib/Ava/Ava.h
+++ lib/Ava/Ava.h
@@ -22,12 +22,12 @@ extern Motor
     Left_Arm_Rotator, Left_Arm_Abductor, Right_Leg_Rotator, Right_Ankle,                                                   // 1-5
     Right_Arm_Abductor, Left_Shoulder, Right_Leg_Abductor_Side_To_Side, Left_Leg_Abductor_Side_To_Side, Right_Arm_Rotator, // 6-10
     Right_Elbow, Left_Knee, Left_Ankle, Left_Leg_Rotator, Right_Shoulder,                                                  // 11-15
-    Back_Chest, Front_Chest, Hips_Rotate_Upper_Body, Right_Knee, Hips_Lean_Side_To_Side,                                   // 17 - 21
+    Hips_Rotate_Upper_Body, Right_Knee, Hips_Lean_Side_To_Side,                                   // 17 - 21
     Hips_Bend_Over, Right_Wrist_Abductor, Left_Wrist_Abductor, Bottom_Neck, Top_Neck,                                      // 22, 25-28
     Left_Leg_Abductor_Front_To_Back, Right_Leg_Abductor_Front_To_Back;
 
 // Ordered in motors array by id (first value of struct)
-// FYI ids 16, 23-24, and 29 are unused
+// FYI ids 16, 23-24, and 29 are unused, 17 and 18 no longer used
 extern int motorsLen;
 extern Motor motors[MOTOR_COUNT];
 
